d4677722a3c8f765490f616b5c028365a843ca06
Jaivignesh Venugopal <smugs1995@gmail.com>
2020-09-20 23:41:56 +0800

    fully implement save, save_as and open

--- pre_ui.py
+++ pre_ui.py
@@ -22,33 +22,44 @@ class UI(QMainWindow):
         self.___open = self.__file.addAction('Open')
         self.___open.setShortcut("Ctrl+O")
         self.___save = self.__file.addAction('Save')
+        self.___save.setShortcut("Ctrl+S")
         self.___save_as = self.__file.addAction('Save As...')
-        self.___save_as.setShortcut("Ctrl+S")
         self.___exit = self.__file.addAction('Exit')
 
         self.configure_menubar_connections()
 
     def configure_menubar_connections(self):
-        self.___save_as.triggered.connect(self.handle_save_as_action)
         self.___open.triggered.connect(self.handle_open_action)
+        self.___save.triggered.connect(self.handle_save_action)
+        self.___save_as.triggered.connect(self.handle_save_as_action)
 
     def handle_open_action(self):
         file_info = QFileDialog.getOpenFileName(self, 'Open File')
-        name, file_type = file_info[0], file_info[1]
+        name, file_type = str(file_info[0]), file_info[1]
         if name != '':
             self.file_path = name
-            file = open(name, 'r')
+            file = open(name, 'r', encoding="utf8")
             with file:
                 text = file.read()
                 self.editor.text = text
+                file.close()
+
+    def handle_save_action(self):
+        if self.file_path:
+            text = self.editor.textForSaving()
+            file = open(self.file_path, 'w', encoding="utf8")
+            file.write(text)
+            file.close()
+        else:
+            self.handle_save_as_action()
 
     def handle_save_as_action(self):
         file_info = QFileDialog.getSaveFileName(self, 'Save As...')
-        name, file_type = file_info[0], file_info[1]
-
+        name, file_type = str(file_info[0]), file_info[1]
         if name != '':
+            self.file_path = name
             text = self.editor.textForSaving()
-            file = open(name[0], 'w')
+            file = open(name, 'w', encoding="utf8")
             file.write(text)
             file.close()
 
