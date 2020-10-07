21db36aad8662c672e96e77b1bc96c48bab7fd4c
Jaivignesh Venugopal <smugs1995@gmail.com>
2020-10-08 00:50:27 +0800

    first step towards a interactive graph

--- graph.py
+++ graph.py
@@ -7,6 +7,9 @@ from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as Navigatio
 import networkx as nx
 import control
 
+from grave import plot_network
+from grave.style import use_attributes
+
 
 class PrettyWidget(QWidget):
 
@@ -15,8 +18,33 @@ class PrettyWidget(QWidget):
         self.index = None
         self.figure = plt.figure()
         self.canvas = FigureCanvas(self.figure)
-
+        self.canvas.mpl_connect('pick_event', self.hilighter)
         self.configure_layout()
+
+    def hilighter(self, event):
+        # if we did not hit a node, bail
+        if not hasattr(event, 'nodes') or not event.nodes:
+            return
+
+        # pull out the graph,
+        graph = event.artist.graph
+
+        # clear any non-default color on nodes
+        for node, attributes in graph.nodes.data():
+            attributes.pop('color', None)
+
+        for u, v, attributes in graph.edges.data():
+            attributes.pop('width', None)
+
+        for node in event.nodes:
+            graph.nodes[node]['color'] = 'C1'
+
+            for edge_attribute in graph[node].values():
+                edge_attribute['width'] = 3
+
+        # update the screen
+        event.artist.stale = True
+        event.artist.figure.canvas.draw_idle()
 
     def configure_layout(self):
         grid = QHBoxLayout()
@@ -35,25 +63,28 @@ class PrettyWidget(QWidget):
         nodes = []
         root = self.index['root']
         curr = self.index['current']
-        colors = []
+
         for key, values in self.index.items():
             if key != 'root' and key != 'current':
                 nodes.append(key)
-                if key == root:
-                    colors.append('#fed8b1')
-                elif key == curr:
-                    colors.append('#32CD32')
-                else:
-                    colors.append('#add8e6')
                 for val in values:
                     edges.append((key, val))
 
         graph.add_nodes_from(nodes)
         graph.add_edges_from(edges)
-        pos = nx.circular_layout(graph)
-        nx.draw(graph, pos=pos, node_color=colors, arrows=True)
-        self.canvas.draw_idle()
+        for node, node_attrs in graph.nodes(data=True):
+            if node == root:
+                node_attrs['color'] = '#fed8b1'
+            elif node == curr:
+                node_attrs['color'] = '#32CD32'
+            else:
+                node_attrs['color'] = '#add8e6'
+            node_attrs['size'] = 200
 
+        plot = plot_network(graph, layout="spectral", node_style=use_attributes(),
+                           edge_style=use_attributes())
+        plot.set_picker(10)
+        self.canvas.draw_idle()
 
 if __name__ == '__main__':
     import sys  
